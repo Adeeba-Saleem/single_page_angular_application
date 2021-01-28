c9daf5b7d20807536312ef8639eb09c79e9a30f1
Adeeba-Saleem <adeeadee2345@gmail.com>
2021-01-28 17:09:42 +0530

    feedbackform

---
+++ src/app/shared/feedback.ts
@@ -0,0 +1,12 @@
+export class Feedback{
+    firstname: string | undefined;
+    lastname: string | undefined;
+    telnum: number | undefined;
+    email: string | undefined;
+    agree: boolean | undefined;
+    contacttype:string | undefined;
+    message: string | undefined;
+
+}
+
+export const ContactType = ['None','Tel','Email'];
